41f1d7ce0337faa158e92aa7f54a6da5e29ea948
Anton Nemtsev <nemtsev.online@gmail.com>
2021-01-14 17:26:11 +0300

    Add files Main and Report

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int balance = 2000_000_000;
+        int transfer = 500_000_000;
+        int total = balance + transfer;
+        System.out.println(total);
+    }}
